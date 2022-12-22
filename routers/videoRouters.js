70c6c26decf324b2c8799aea5772a4492db7a57c
udit khandelwal <u.khandelwal02@gmail.com>
2022-12-22 19:09:47 +0530

    Update videoRouters.js

--- routers/videoRouters.js
+++ routers/videoRouters.js
@@ -3,8 +3,8 @@ const videoController = require('../controllers/videoController');
 
 const routes = express.Router();
 
+routes.get('/video',videoController.streamAPI);
 routes.get('/shows', videoController.listAPI);
 routes.get('/shows/:id', videoController.detailAPI);
-routes.get('/stream/:id', videoController.streamAPI)
 
 module.exports = routes;
