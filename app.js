c562b1ead83bd13124b54e4e0ec7349c31a250e8
udit khandelwal <u.khandelwal02@gmail.com>
2022-12-22 19:10:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const videoRouter = require("./Routes/videoRouters");
 const server = express();
 
-
 const PORT = 9090;
 const URI = "mongodb://localhost:27017/video";
+const staticPath = path.join(__dirname,'public');
 
 mongoose.connect(URI,{
     useNewUrlParser: true,
@@ -20,7 +21,13 @@ mongoose.connect(URI,{
         }
     });
 
+
 server.use(bodyParser.json());
+server.use(express.static(staticPath));
+server.get('/',(req,res)=>{
+    res.sendFile(path.join(staticPath,'app.html'));
+})
+
 server.use('/', videoRouter);
 
 server.listen(PORT,() => {console.log(`server started listenig at ${PORT}`)});
